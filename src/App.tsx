955421405e2ebb1bcbf4b22c73c4f8d71c9fea65
Romain B. (Neyticom) <romainb.boitepro@gmail.com>
2025-06-11 15:00:43 +0200

    init : default application structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/icon.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="#">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>La ruche des pros</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Tu as cliqué {count} fois :o
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Notre site arrive bientôt et ça va être une vraie singerie :P
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Le futur du BTP se dessine aujourd'hui
       </p>
     </>
   )
